0e5cf2f8b5624cec1b2b541ee87bb42536572485
Gustavofontenele1 <gustavofontenele@live.com>
2022-03-05 13:29:00 -0300

    add 2 card

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
 const itens = [
     {
-        text: "",
         texto: "NEF",
         number: '5',
         value: 25,
     },
     {
-        text: "",
         texto: "PED",
         number: '1',
         value: 30,
@@ -16,11 +14,37 @@ const itens = [
 let itens2 = itens.map(function (element) {
 
     return `<div class="progress" style="background: conic-gradient(#00f5d2 ${element.value}%, #202d2e ${element.value}%)">
-                <span class="progress-value">${element.text}
-                <p class="progress-number" style="margin-bottom: -87px">${element.texto}</p>   
+                <span class="progress-value active">
+                <p class="progress-number">${element.texto}</p>   
                     <p class="progress-number">${element.number}</p>    
                 </span>
             </div>
             `
 });
 document.querySelector(".teste3").innerHTML = itens2.join('\n');
+const itens6 = [
+    {
+        letra: "REU",
+        num: '2',
+        valor: 25,
+    },
+    {
+        letra: "OBS",
+        num: '2',
+        valor: 30,
+    },
+
+]
+let itens4 = itens6.map(function (elemento) {
+
+    return `<div class="progress1" style="background: conic-gradient(#00f5d2 ${elemento.valor}%, #202d2e ${elemento.valor}%)">
+                <span class="progress-value1">
+                <p class="progress-number">${elemento.letra}</p>   
+                    <p class="progress-number">${elemento.num}</p>    
+                </span>
+            </div>
+            `
+});
+document.querySelector(".test3").innerHTML = itens4.join('\n');
+
+
